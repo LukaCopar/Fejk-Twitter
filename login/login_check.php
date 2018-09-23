b3716c66324483d3c72d62e6d346f4671a587af9
Luka Čopar <luka.copar@gmail.com>
2018-09-23 16:42:58 +0200

    secure login naret

--- login/login_check.php
+++ login/login_check.php
@@ -9,20 +9,19 @@ $password = $_POST['pass'];
 
    $ime = $pdo->prepare('SELECT * FROM users WHERE (username = ?)AND(password = ?)');
    $ime->execute([$ussername, $password]);
-   $nevemvec = $ime->fetch();
-   foreach ($ime as $row)
-   echo $row['name'];
-             die();
-   if(isset($nevemvec) > 0){
+   $nevemvec = $ime->rowcount();
+   if($nevemvec == 1){
             if(!isset($_COOKIE[$cookie_login])>0){
+                $cookie_value = $ussername;
                 
                   setcookie($cookie_login, $cookie_value,time() + (10*999999),"/");
                   header('Location: ../index.php');
+                  die();
             } 
 
-        }
+        }else{
               
               
             header('Location: ./login.php?id=1');
-            
+        }
         
